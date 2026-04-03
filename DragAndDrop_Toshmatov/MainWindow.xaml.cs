51b9a307dc9cb81a3d17ee6de249f684dbe60ba5
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-04-03 12:02:54 +0500

    Создание таймера. Тошматов

--- DragAndDrop_Toshmatov/MainWindow.xaml.cs
+++ DragAndDrop_Toshmatov/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Threading;
 
 
 namespace DragAndDrop_Toshmatov
@@ -6,10 +7,19 @@ namespace DragAndDrop_Toshmatov
 
     public partial class MainWindow : Window
     {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+
         public MainWindow()
         {
             InitializeComponent();
+            dispatcherTimer.Tick += DispatcherTimer_Tick; 
+            dispatcherTimer.Interval = new System.TimeSpan(0, 0, 0, 0, 1000 / 60); 
         }
+        private void DispatcherTimer_Tick(object sender, System.EventArgs e)
+        {
+
+        }
+
         private void image_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
 
