337af29797bd8f4619e6efbf358363caeada26a5
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-04-03 11:56:04 +0500

    Настройка событий. Тошматов

--- DragAndDrop_Toshmatov/MainWindow.xaml.cs
+++ DragAndDrop_Toshmatov/MainWindow.xaml.cs
@@ -1,24 +1,22 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
 
 namespace DragAndDrop_Toshmatov
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void image_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
+        }
+        private void Image_MouseUDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
         }
     }
 }
