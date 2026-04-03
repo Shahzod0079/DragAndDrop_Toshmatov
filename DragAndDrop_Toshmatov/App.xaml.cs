8d9dbf8e4f6ba0a3c33990d954768cefe1f3a343
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-04-03 11:42:03 +0500

    Добавьте файлы проекта.

---
+++ DragAndDrop_Toshmatov/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DragAndDrop_Toshmatov
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
