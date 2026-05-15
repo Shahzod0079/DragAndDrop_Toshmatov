bb938ca324845902726acdd37d3c6d6a3e5a5695
student-v424 <student-v424@STUD-RM6-V424>
2026-05-15 14:36:17 +0300

    Правки

--- DragAndDrop_Toshmatov/Inventory.xaml.cs
+++ DragAndDrop_Toshmatov/Inventory.xaml.cs
@@ -10,10 +10,44 @@ namespace DragAndDrop_Toshmatov
     {
         private Image? draggedImage;
         private Border? sourceBorder;
+        private int currentFilledSlots = 56;
+        private int currentMoney = 14633098;
 
         public Inventory()
         {
             InitializeComponent();
+            UpdateSlotsDisplay();
+            UpdateMoneyDisplay();
+        }
+
+        private void UpdateSlotsDisplay()
+        {
+            SlotsText.Text = $"{currentFilledSlots}/80";
+        }
+
+        private void UpdateMoneyDisplay()
+        {
+            MoneyText.Text = currentMoney.ToString("N0");
+        }
+
+        //  уменьшаем деньги при перетаскивании
+        private void UpdateMoney(int amount)
+        {
+            currentMoney = amount;
+            UpdateMoneyDisplay();
+        }
+
+        // Добавляем или убавляем деньги
+        private void AddMoney(int amount)
+        {
+            currentMoney += amount;
+            UpdateMoneyDisplay();
+        }
+
+        private void SubtractMoney(int amount)
+        {
+            currentMoney -= amount;
+            UpdateMoneyDisplay();
         }
 
         private void Item_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -56,14 +90,35 @@ namespace DragAndDrop_Toshmatov
             {
                 if (sourceBorder != null && sourceBorder != targetBorder)
                 {
+                    // Обновляем слоты
+                    if (targetBorder.Child == null && sourceBorder.Child != null)
+                    {
+                        currentFilledSlots++;
+                    }
+                    else if (targetBorder.Child != null && sourceBorder.Child == null)
+                    {
+                        currentFilledSlots--;
+                    }
+
                     var temp = sourceBorder.Child;
                     sourceBorder.Child = targetBorder.Child;
                     targetBorder.Child = temp;
+
+                    UpdateSlotsDisplay();
+
+                    // Обновляем деньги при пеертаскивании
+                    SubtractMoney(1000);
                 }
                 targetBorder.Background = new SolidColorBrush(Color.FromRgb(58, 36, 24));
                 draggedImage = null;
                 sourceBorder = null;
             }
+        }
+
+        // Клик по золоту - добавляем деньги
+        private void OnGoldClicked(object sender, MouseButtonEventArgs e)
+        {
+            AddMoney(5000);
         }
 
         private void CloseWindow(object sender, RoutedEventArgs e) => this.Close();
